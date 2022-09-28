aeac4b2fa93f49f5fe00f1c4b5bdc867f659c73a
Yuting Xie <yuting.xie@aalto.fi>
2022-09-29 00:11:41 +0300

    T.2.1 first solution

--- Module_2/T1-first_touch/src/first.cpp
+++ Module_2/T1-first_touch/src/first.cpp
@@ -6,4 +6,5 @@ void Hello() {
   // Write here the line that prints "Hello world!" and a newline.
   // Hint: if your output is empty in the tests, remember the part about
   // flushing!
+    std::cout << "Hello world!" << std::endl;
   }
