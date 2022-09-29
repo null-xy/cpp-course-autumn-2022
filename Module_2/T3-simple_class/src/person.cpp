ba8ab7b3a228a2a26bb3bd66a9f337c89f94f3e9
Yuting Xie <yuting.xie@aalto.fi>
2022-09-29 03:04:46 +0300

    T.2.3 first solution

--- Module_2/T3-simple_class/src/person.cpp
+++ Module_2/T3-simple_class/src/person.cpp
@@ -2,3 +2,14 @@
 
 // define your Person class' functions here
 
+Person::Person(const std::string& name, const int birthyear)
+: name_(name),birthyear_(birthyear),age_(0){
+}
+
+const std::string& Person::GetName() const{
+    return name_;
+}
+int Person::GetAge(int currentyear){
+    age_= currentyear - birthyear_;
+    return age_;
+}
--- Module_2/T3-simple_class/src/person.hpp
+++ Module_2/T3-simple_class/src/person.hpp
@@ -2,6 +2,18 @@
 #define AALTO_ELEC_CPP_PERSON
 
 // declare your Person class here
+#include <string>
 
+class Person{
+public:
+    Person(const std::string& name, const int birthyear);
 
+    const std::string& GetName() const;
+    int GetAge(int currentyear);
+private:
+    std::string name_;
+    int birthyear_;
+    int age_;
+
+};
 #endif
