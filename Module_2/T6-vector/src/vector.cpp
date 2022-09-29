c2d1585641a9a74633a967d76d9084510064ea4a
Yuting Xie <yuting.xie@aalto.fi>
2022-09-29 19:17:06 +0300

    T.2.6 first solution

--- Module_2/T6-vector/src/vector.cpp
+++ Module_2/T6-vector/src/vector.cpp
@@ -1,18 +1,18 @@
 #include "vector.hpp"
-
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 int GetMin(std::vector<int> v) {
-  
-  return 0; 
+  return *std::min_element(v.begin(), v.end()); 
 }
 
 int GetMax(std::vector<int> v) {
-  
-  return 0; 
+  return *std::max_element(v.begin(), v.end()); 
 }
 
 double GetAvg(std::vector<int> v) {
-  
-  return 0.0; 
+  double sum=std::accumulate(v.begin(), v.end(),0.0);
+  int count=v.size();
+  return sum/count; 
 }
