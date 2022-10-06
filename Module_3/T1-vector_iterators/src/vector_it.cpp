056ab7ca4cd33518c4a548bcd1a4fef67f24284f
Yuting Xie <yuting.xie@aalto.fi>
2022-10-06 11:14:39 +0300

    T.3.1 first solution

--- Module_3/T1-vector_iterators/src/vector_it.cpp
+++ Module_3/T1-vector_iterators/src/vector_it.cpp
@@ -16,20 +16,16 @@ void PrintSum1(const std::vector<int>& v) {
   int sum_1;
   for (std::vector<int>::const_iterator it = v.begin(); it != v.end()-1; it++) {
     sum_1=*it+*(it+1);
-    if(it<v.end()-2){
-      std::cout << sum_1 <<" ";
-    }else 
-    std::cout << sum_1 << std::endl;
+    std::cout << sum_1 <<" ";
   }
+  std::cout << std::endl;
 }
 
 void PrintSum2(const std::vector<int>& v) {
   int sum_2;
  for(unsigned i = 0; i< v.size()/2; ++i) {
       sum_2=v[i]+v[v.size()-i-1];
-      if(i<v.size()/2 -1){
-        std::cout << sum_2 <<" ";
-      }else
-      std::cout << sum_2 << std::endl;
+      std::cout << sum_2 <<" ";
     }
+    std::cout << std::endl;
 }
