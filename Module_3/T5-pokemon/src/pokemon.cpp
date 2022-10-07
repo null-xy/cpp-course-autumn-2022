f2bc9e9e5d363a0437f591642b621edb4f57038c
Yuting Xie <yuting.xie@aalto.fi>
2022-10-07 14:32:03 +0300

    environment debug

--- Module_3/T5-pokemon/src/pokemon.cpp
+++ Module_3/T5-pokemon/src/pokemon.cpp
@@ -1,3 +1,28 @@
 #include "pokemon.hpp"
 
 
+void PokemonCollection::Add(const std::string& name, size_t id){
+    pokemons_.push_back(std::make_pair(name,id));
+}
+
+bool PokemonCollection::Remove(const std::string& name, size_t id){
+
+}
+
+void PokemonCollection::Print() const{
+    for(auto it=pokemons_.begin();it !=pokemons_.end();it++){
+        std::cout << "id: " <<it ->second
+        <<", name: "<<it ->first<< std::endl;
+    }
+    /*for(std::list<std::pair<std::string, size_t>>::iterator it=pokemons_.begin(); it!=pokemons_.end(); it++){
+        std::cout << "id: " <<it ->second
+        <<", name: "<<it ->first<< std::endl;
+    }*/
+}
+
+void PokemonCollection::SortByName(){
+
+}
+void PokemonCollection::SortById(){
+
+}
